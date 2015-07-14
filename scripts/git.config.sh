0d3dfaa48cb26be2d39f5c8414d53d29717e41aa
Michael Cannon <michael@axelerant.com>
2015-07-14 09:15:50 -0400

    Update gitconfig

--- scripts/git.config.sh
+++ scripts/git.config.sh
@@ -1,4 +1,4 @@
 git config --global user.name "Michael Cannon"
-git config --global user.email "michael@typo3vagabond.com"
+git config --global user.email "michael@axelerant.com"
 git config --global color.ui auto
 git config --global pager.status yes
