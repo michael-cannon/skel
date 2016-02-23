809351ac3ced6243ccf3ca672a278afbf80ab46d
Michael Cannon <michael@axelerant.com>
2016-02-23 15:51:32 +0800

    Minor Capture One helpers added

--- bin/mysql_import.sh
+++ bin/mysql_import.sh
@@ -4,11 +4,10 @@
 #
 # @author Michael Cannon, mc@aihr.us
 
-# five args
-if [ ! ${5} ]
+if [ ! ${4} ]
 then
 	FUNCTION=`basename $0`
-	echo "Usage: ${FUNCTION} host user password database sql-file"
+	echo "Usage: ${FUNCTION} host user database sql-file"
 	echo Simple MySQL database script loader
 	exit
 fi
@@ -16,6 +15,5 @@ fi
 nice mysql \
 	--host=${1} \
 	--user=${2} \
-	--password="${3}" \
 	--default-character-set=utf8 \
-	"${4}" < ${5}
+	"${3}" < ${4}
